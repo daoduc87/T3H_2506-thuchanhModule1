0267b957ad735185d3e6d6bd3c02209d6a905e75
daoduc87 <daotienduc0703@gmail.com>
2025-08-07 22:06:53 +0700

    update

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ const videoContainer = document.querySelector('#video-container');
 button.addEventListener('click', () => {
     videoContainer.innerHTML = `
     <div class="relative">
-    <iframe width="853" height="480" src="https://www.youtube.com/embed/pIuqh7GP308?autoplay=1" class="w-full sm:w-[760px] sm:h-[480px] lg:w-[850px] lg:h-[480px] sm:aspect-video"
+    <iframe width="853" height="480" src="https://www.youtube.com/embed/3Njxv_D3MEQ?si=w8auIxrUcwi5LKF2" class="w-full sm:w-[760px] sm:h-[480px] lg:w-[850px] lg:h-[480px] sm:aspect-video" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     title="YouTube video player" frameborder="0"
     allow="autoplay; encrypted-media" allowfullscreen></iframe>
     <button id="close-video" class="absolute top-[-20px] right-[-20px] text-white text-2xl">
